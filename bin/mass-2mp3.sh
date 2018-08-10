04cb544778e03188d7e2affeca8302b69d10e7ca
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2018-08-10 23:42:03 +0300

    added examples of find use

--- bin/mass-2mp3.sh
+++ bin/mass-2mp3.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-# script for mass cue to mp3 covretor generation
+# script for mass file to mp3 covretor generation
+
+# USE CASE1 (only current dir processed):
+# mass-2mp3.sh *.m4a
+
+# USE CASE 2 (all found files processed)
+# find . -name '*.m4a' -exec mass-2mp3.sh {} +
+
+# info: working with file name parts in bash
+# pax> a=/tmp/xx/file.tar.gz
+# pax> xpath=${a%/*}
+# pax> xbase=${a##*/}
+# pax> xfext=${xbase##*.}
+# pax> xpref=${xbase%.*}
+# pax> echo;echo path=${xpath};echo pref=${xpref};echo ext=${xfext}
+# path=/tmp/xx
+# pref=file.tar
+# ext=gz
 
 script_name='cnv2mp3.sh'
 
@@ -9,8 +26,9 @@ echo '#!/bin/bash' > $script_name
 for file in "$@"
 do
     fname=${file##*/}  #file name without path
+    fpath=${file%/*}   #file path only
     fbname=${fname%.*} #base name without extention
-    echo ffmpeg -i "'$file'" -codec:a libmp3lame -qscale:a 1 "'$fbname.mp3'" >> $script_name
+    echo ffmpeg -i "'$file'" -codec:a libmp3lame -qscale:a 1 "'$fpath/$fbname.mp3'" >> $script_name
 done
 
 chmod +x $script_name
