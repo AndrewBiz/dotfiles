9e4235308c9ff81323e06937407f112b212bdaf6
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2018-08-21 00:07:55 +0300

    improved script

--- bin/process-day-photos.sh
+++ bin/process-day-photos.sh
@@ -7,19 +7,29 @@
 
 # arrange and rename files:
 # !!!
-# phls -R | phmove . -a | phrename -a anb
-
+echo "___ STEP 1: moving and renaming files in current dir"
+phls -R | phmove . -a | phrename -a anb
+echo "___."
 # find _clock_ files and calculate time difference (UTC - photo) in seconds
-
+echo "Find _clock_ files, calculate time delta (UTC-photo_time) and put in the script"
+read -p "___ PAUSED (press enter to continue)"
 # rename to get correct date-time in file-names
 # !!!
-#phls -R | phrename -s -194
+echo "___ STEP 2: changing date-time-in-the-name"
+phls -R | phrename -s -4
+echo "___."
 
 # fix dto and fmd
+echo "___ STEP 3: fixing file_modify_date"
 phls -R | phfixfmd
-#phls -R | phfixdto
+echo "___."
+echo "___ STEP 4: fixing DateTimeOriginal tag"
+phls -R | phfixdto
+echo "___."
 
 # put geo tags in files
 # geotag0300home .
 # !!!
+echo "___ STEP 5: Adding geo-tags"
 exiftool -preserve -overwrite_original -geotag "./geotag/*.*" '-geotime<${DateTimeOriginal}+03:00' .
+echo "___."
